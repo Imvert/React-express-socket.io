d76110a62e8a9557857b9300e077f296e18cb2c7
LUIS ALEXANDER PILATUÑA LOPEZ <luis.pilatuna.lopez@estudiantes.cordillera.edu.ec>
2024-01-03 13:17:57 -0500

    add url to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io(
+  "https://chat-app-crsw.onrender.com/" || "http://localhost:4000"
+);
 function App() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
